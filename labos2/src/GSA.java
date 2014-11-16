abedfd3ceefdeac54bb3a7aace0b5f5f55987dcd
Paula <gombarica@gmail.com>
2014-11-16 16:46:44 +0100

    Ocisceno.

--- labos2/src/GSA.java
+++ labos2/src/GSA.java
@@ -30,7 +30,7 @@ public class GSA {
 	private static void parseInput() throws IOException {
 		// BufferedReader br = new BufferedReader(new
 		// InputStreamReader(System.in));
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("testSloz.san")));
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.san")));
 		String currLine;
 
 		// input nonterminals
@@ -134,17 +134,16 @@ public class GSA {
 		BeginsWithTable beginsWith = new BeginsWithTable(allChars, nonterminals, grammar);
 		EpsilonNKA eNKA = new EpsilonNKA(nonterminals.get(0), grammar, beginsWith, nonterminals, terminals);
 		eNKA.generateEpsilonNKA();
-		// eNKA.outputStates();
-		// System.out.println("---------------------------------------------------------------");
-		// eNKA.outputTransitions();
-		// System.out.println("---------------------------------------------------------------");
 		DKA dka = new DKA(eNKA);
+
 		dka.outputClusters();
 		System.out.println("---------------------------------------------------------------");
 		dka.outputTransitions();
 		System.out.println("---------------------------------------------------------------");
+
 		LRParserTable = new GenerateLRParserTable(dka, terminals, nonterminals);
 		System.out.println(LRParserTable.toString());
+
 		outputCollections();
 	}
 
