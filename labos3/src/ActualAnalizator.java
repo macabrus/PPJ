f511dc7acc84cfd61229143da9bed5f1cc05ce3c
Paula <gombarica@gmail.com>
2014-12-13 18:54:58 +0100

    ntip nije isto kao i tip!!!

--- labos3/src/ActualAnalizator.java
+++ labos3/src/ActualAnalizator.java
@@ -1,182 +1,333 @@
-
 /**
  * The class that actually analyzes (?)
  * @author Ivan
- *
+ * 
  */
 public class ActualAnalizator {
-	
+
 	private TreeNode root;
 	private TableNode scope;
-	
-	private boolean error = false; 
-	
+
+	private boolean error = false;
+
 	/**
 	 * Constructs class from root of generative tree
 	 * @param root Root of generative tree
 	 */
-	public ActualAnalizator(TreeNode root){
+	public ActualAnalizator(TreeNode root) {
 		this.root = root;
 		scope = new TableNode();
 	}
-	
+
 	public void analyze() {
 		prijevodnaJedinica(root);
-	} 
-	
+	}
+
 	private void prijevodnaJedinica(TreeNode node) {
 		if (node.getChildren().size() == 1) {
-			vanjskaDeklaracija(node.getChildAt(0)); if (error) return;
+			vanjskaDeklaracija(node.getChildAt(0));
+			if (error)
+				return;
 		} else {
-			prijevodnaJedinica(node.getChildAt(0)); if (error) return;
-			vanjskaDeklaracija(node.getChildAt(1)); if (error) return;
-	
+			prijevodnaJedinica(node.getChildAt(0));
+			if (error)
+				return;
+			vanjskaDeklaracija(node.getChildAt(1));
+			if (error)
+				return;
+
 		}
 	}
-	
+
 	private void vanjskaDeklaracija(TreeNode node) {
 		if (node.getChildAt(0).getContent().equals("<definicija_funkcije>")) {
-			definicijaFunkcije(node.getChildAt(0)); if (error) return;
+			definicijaFunkcije(node.getChildAt(0));
+			if (error)
+				return;
 		} else {
-			deklaracija(node.getChildAt(0)); if (error) return;
+			deklaracija(node.getChildAt(0));
+			if (error)
+				return;
 		}
 	}
-	
+
 	private void definicijaFunkcije(TreeNode node) {
-		
+
 		imeTipa(node.getChildAt(0));
-		
+
 		if (node.getChildAt(0).isConst()) {
 			printErrorMessage(node);
 			return;
 		}
-	
-		if (existsFunctionBefore(scope, node.getFunctionName(), node.getType())){
+
+		if (existsFunctionBefore(scope, node.getFunctionName(), node.getType())) {
 			printErrorMessage(node);
 			return;
 		}
-		
+
 		if (node.getChildAt(3).getContent().equals("KR_VOID")) {
-			
+
 			if (conflictingDeclaration(scope, node.getFunctionName(), node.getType())) {
 				printErrorMessage(node);
 				return;
 			}
-		
+
 			node.setDefined(true);
 			node.setType(node.getChildAt(0).getType());
-			
-			slozenaNaredba(node.getChildAt(6)); if (error) return;
-			
+
+			slozenaNaredba(node.getChildAt(6));
+			if (error)
+				return;
+
 			scope.addChild(node);
-			
-			
+
 		} else {
 
-			listaParametara(node.getChildAt(3)); if (error) return;
-			
+			listaParametara(node.getChildAt(3));
+			if (error)
+				return;
+
 			if (conflictingDeclaration(scope, node.getFunctionName(), node.getType())) {
 				printErrorMessage(node);
 				return;
 			}
-			
+
 			node.setDefined(true);
 			node.setType(node.getChildAt(0).getType());
-			
+
 			node.setTypes(node.getChildAt(3).getTypes());
 			node.setNames(node.getChildAt(3).getNames());
-			
+
 			scope.addChild(node);
-			slozenaNaredba(node.getChildAt(5)); if (error) return;
-		
+			slozenaNaredba(node.getChildAt(5));
+			if (error)
+				return;
+
 		}
-		
+
 	}
-	
+
 	private void deklaracija(TreeNode node) {
-		imeTipa(node.getChildAt(0)); 
+		imeTipa(node.getChildAt(0));
+		if (error)
+			return;
 		node.getChildAt(1).setType(node.getChildAt(0).getType());
 		listaInitDeklaratora(node.getChildAt(1));
 	}
-	
+
 	private void imeTipa(TreeNode node) {
 		specifikatorTipa(node.getChildAt(0));
 		if (node.getChildren().size() == 1) {
 			node.setType(node.getChildAt(0).getType());
 		} else {
 			TreeNode specTip = node.getChildAt(0);
-			if (specTip.getType().equals("void")) printErrorMessage(node); if (error) return;
+			if (specTip.getType().equals("void"))
+				printErrorMessage(node);
+			if (error)
+				return;
 			node.setType(node.getChildAt(0).getType());
 			node.setConst();
 		}
 	}
-	
+
 	private void slozenaNaredba(TreeNode node) {
-		
+
 		TableNode copyOfScope = new TableNode(scope.getParent());
 		copyOfScope.setDeclaredStuff(scope.getDeclaredStuff());
-		
+
 		TableNode newScope = new TableNode(copyOfScope);
 		scope = newScope;
-		
+
 		// gadna brija neka
-		
+
 		if (node.getChildren().size() == 3) {
-			listaNaredbi(node.getChildAt(1)); if (error) return;
+			listaNaredbi(node.getChildAt(1));
+			if (error)
+				return;
 		} else {
-			listaDeklaracija(node.getChildAt(1)); if (error) return;
-			listaNaredbi(node.getChildAt(2)); if (error) return;
+			listaDeklaracija(node.getChildAt(1));
+			if (error)
+				return;
+			listaNaredbi(node.getChildAt(2));
+			if (error)
+				return;
 		}
-		
+
 		scope = scope.getParent();
-		
+
 	}
-	
+
+	private void deklaracijaParametra(TreeNode node) {
+		imeTipa(node);
+		if (error)
+			return;
+		if (node.getChildAt(0).getType().equals("void")) {
+			printErrorMessage(node);
+		}
+		// postavi tip i ime
+		if (node.getChildren().size() == 2) {
+			node.setType(node.getChildAt(0).getType());
+		} else {
+			node.setType("niz" + node.getChildAt(0).getType());
+		}
+		node.setName(node.getChildAt(1).getName());
+	}
+
 	private void listaParametara(TreeNode node) {
 		if (node.getChildren().size() == 1) {
-			deklaracijaParametara(node.getChildAt(0)); if (error) return;
+			deklaracijaParametra(node.getChildAt(0));
+			if (error)
+				return;
 			node.addType(node.getChildAt(0).getType());
 			node.addName(node.getChildAt(0).getName());
 		} else {
-			
-			listaParametara(node.getChildAt(0)); if (error) return;
-			deklaracijaParametara(node.getChildAt(2)); if (error) return;
-			
-			if(node.getChildAt(0).getNames().contains(node.getChildAt(2).getName())) {
+
+			listaParametara(node.getChildAt(0));
+			if (error)
+				return;
+			deklaracijaParametra(node.getChildAt(2));
+			if (error)
+				return;
+
+			if (node.getChildAt(0).getNames().contains(node.getChildAt(2).getName())) {
 				printErrorMessage(node);
 				return;
 			}
-				
+
 			node.setTypes(node.getChildAt(0).getTypes());
 			node.addType(node.getChildAt(2).getType());
-		
+
 			node.setNames(node.getChildAt(0).getNames());
 			node.addName(node.getChildAt(2).getName());
-		
+
 		}
-	}	
-	
+	}
+
+	private void initDeklarator(TreeNode node) {
+		// nasljedno svojstvo prvo
+		node.getChildAt(0).setType(node.getType());
+		izravniDeklarator(node);
+		if (error)
+			return;
+		if (node.getChildren().size() == 1) {
+			// sad tu treba biti prvoDijete.tip =/= const
+			// "vazno je uociti da se provjerava izvedeno svojstvo tip, a ne nasljedno svojstvo ntip"
+			// znaci da ih treba razlikovati -.-
+		}
+	}
+
 	private void listaInitDeklaratora(TreeNode node) {
 		if (node.getChildren().size() == 1) {
 			node.getChildAt(0).setType(node.getType());
-			initDeklarator(node.getChildAt(0)); if (error) return;
+			initDeklarator(node.getChildAt(0));
+			if (error)
+				return;
 		} else {
-			
+
 			node.getChildAt(0).setType(node.getType());
-			listaInitDeklaratora(node.getChildAt(0)); if (error) return;
-			
+			listaInitDeklaratora(node.getChildAt(0));
+			if (error)
+				return;
+
 			node.getChildAt(2).setType(node.getType());
-			initDeklarator(node.getChildAt(2)); if (error) return;
-			
+			initDeklarator(node.getChildAt(2));
+			if (error)
+				return;
+
 		}
 	}
-	
+
 	private void specifikatorTipa(TreeNode node) {
-		if (node.getChildAt(0).equals("KR_VOID")) node.setType("void");
-		if (node.getChildAt(0).equals("KR_CHAR")) node.setType("char");
-		if (node.getChildAt(0).equals("KR_INT")) node.setType("int");
+		if (node.getChildAt(0).equals("KR_VOID"))
+			node.setType("void");
+		if (node.getChildAt(0).equals("KR_CHAR"))
+			node.setType("char");
+		if (node.getChildAt(0).equals("KR_INT"))
+			node.setType("int");
 	}
-	
+
+	private void izrazPridruzivanja(TreeNode node) {
+		if (node.getChildren().size() == 1) {
+			logIliIzraz(node.getChildAt(0));
+			if (error)
+				return;
+			node.setType(node.getChildAt(0).getType());
+			node.setlValue(node.getChildAt(0).getlValue());
+		} else {
+			postfiksIzraz(node.getChildAt(0));
+			if (error)
+				return;
+			izrazPridruzivanja(node.getChildAt(2));
+			if (error)
+				return;
+			// provjeri ~
+			if (!isImplicitlyCastable(node.getChildAt(2), node.getChildAt(0).getType())) {
+				printErrorMessage(node);
+			}
+			node.setType(node.getChildAt(0).getType());
+			node.setlValue(false);
+		}
+	}
+
+	private void izraz(TreeNode node) {
+		if (node.getChildren().size() == 1) {
+			izrazPridruzivanja(node.getChildAt(0));
+			if (error)
+				return;
+			node.setType(node.getChildAt(0).getType());
+			node.setlValue(node.getChildAt(0).getlValue());
+		} else {
+			izraz(node.getChildAt(0));
+			if (error)
+				return;
+			izrazPridruzivanja(node.getChildAt(2));
+			if (error)
+				return;
+			node.setType(node.getChildAt(2).getType());
+			// ne znam jel trebamo ovo kao boolean ili int
+			node.setlValue(false);
+		}
+	}
+
+	private void izrazNaredba(TreeNode node) {
+		if (node.getChildren().size() == 1) {
+			node.setType("int");
+		} else {
+			izraz(node.getChildAt(1));
+			if (error)
+				return;
+			node.setType(node.getChildAt(1).getType());
+		}
+	}
+
+	private void naredba(TreeNode node) {
+		if (node.getChildAt(0).equals("<slozena_naredba>")) {
+			slozenaNaredba(node.getChildAt(0));
+			if (error)
+				return;
+		}
+		if (node.getChildAt(0).equals("<izraz_naredba>")) {
+			izrazNaredba(node.getChildAt(0));
+			if (error)
+				return;
+		}
+		if (node.getChildAt(0).equals("<naredba_grananja>")) {
+			naredbaGrananja(node.getChildAt(0));
+			if (error)
+				return;
+		}
+		if (node.getChildAt(0).equals("<naredba_petlje>")) {
+			naredbaPetlje(node.getChildAt(0));
+			if (error)
+				return;
+		}
+		if (node.getChildAt(0).equals("<naredba_skoka>")) {
+			naredbaSkoka(node.getChildAt(0));
+			if (error)
+				return;
+		}
+	}
+
 	private void listaNaredbi(TreeNode node) {
 		if (node.getChildren().size() == 1) {
 			naredba(node.getChildAt(0));
@@ -185,8 +336,8 @@ public class ActualAnalizator {
 			naredba(node.getChildAt(1));
 		}
 	}
-	
-	private void listaDeklaracija(TreeNode node) {ž
+
+	private void listaDeklaracija(TreeNode node) {
 		if (node.getChildren().size() == 1) {
 			deklaracija(node.getChildAt(0));
 		} else {
@@ -194,43 +345,64 @@ public class ActualAnalizator {
 			deklaracija(node.getChildAt(1));
 		}
 	}
-	
+
 	private boolean existsFunctionBefore(TableNode node, String funName, String funType) {
-	
 		TableNode prevNode = node;
 		node = node.getParent();
 		while (node != null) {
 			for (TreeNode d : node.getDeclaredStuff()) {
-				if (d.isFunction() && d.getFunctionName().equals(funName)
-						&& d.isFunctionDefined()) return true;
+				if (d.isFunction() && d.getFunctionName().equals(funName) && d.isFunctionDefined())
+					return true;
 			}
 			prevNode = node;
 			node = node.getParent();
 		}
-	
+
 		return false;
 	}
-	
+
 	private boolean conflictingDeclaration(TableNode node, String funName, String funType) {
-		
-		while (node.getParent() != null) node = node.getParent();
-		
+		while (node.getParent() != null)
+			node = node.getParent();
+
 		for (TreeNode d : node.getDeclaredStuff()) {
-			if (d.isFunction() && d.getFunctionName().equals(funName) && 
-					!d.getType().equals(funType)) return true;
+			if (d.isFunction() && d.getFunctionName().equals(funName) && !d.getType().equals(funType))
+				return true;
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 	/**
 	 * 
 	 * @param node
 	 */
 	private void printErrorMessage(TreeNode node) {
-		System.out.println(node.getContent() + " ::= " +  node);
+		System.out.println(node.getContent() + " ::= " + node);
 		error = true;
 	}
-	
+
+	/**
+	 * Method that returns whether t1 can be implicitly cast to t2.
+	 * @param t1
+	 * @param t2
+	 * @return Possible cast or not
+	 */
+	private boolean isImplicitlyCastable(TreeNode t1, String t2) {
+		// const(T) ~ T
+		if (!t1.getType().startsWith("niz") && t1.isConst()) {
+			return true;
+		}
+		// char ~ int
+		if (t1.getType().equals("char") && t2.equals("int")) {
+			return true;
+		}
+		// niz(T) ~ niz(const(T)), ako T =/= const
+		if (!t1.isConst() && t1.getType().startsWith("niz")) {
+			return true;
+		}
+		return false;
+	}
+
 }
--- labos3/src/TreeNode.java
+++ labos3/src/TreeNode.java
@@ -1,38 +1,37 @@
 import java.util.ArrayList;
 
-
 /**
  * Class which models a single node of a generative tree.
  * 
  * @author Ivan Paljak
  */
 public class TreeNode {
-	
+
 	private int depth;
 	private String content;
 
 	private ArrayList<TreeNode> children;
 
 	// -------------------------------------------
-	
+
 	private boolean lValue;
 	private boolean isConstant;
 	private boolean isDefined;
-	private boolean isConst;
 	private String type;
-	
-	private ArrayList<String> types = new ArrayList<String>(); 
+	private String name;
+
+	private ArrayList<String> types = new ArrayList<String>();
 	private ArrayList<String> names = new ArrayList<String>();
-	
+
 	/**
-	 * Node constructor from its contents 
+	 * Node constructor from its contents
 	 */
 	public TreeNode(int depth, String content) {
 		this.depth = depth;
 		this.content = content;
 		this.children = new ArrayList<TreeNode>();
 	}
-	
+
 	/**
 	 * Getter method for <code>depth</code> field.
 	 * @return depth of node
@@ -40,7 +39,7 @@ public class TreeNode {
 	public int getDepth() {
 		return this.depth;
 	}
-	
+
 	/**
 	 * Getter method for <code>children</code> field
 	 * @return children of node
@@ -48,34 +47,37 @@ public class TreeNode {
 	public ArrayList<TreeNode> getChildren() {
 		return this.children;
 	}
-	
+
 	/**
 	 * Returns child at index
 	 * 
 	 * @param i index
 	 * @return :)
 	 */
-	public TreeNode getChildAt(int i){
+	public TreeNode getChildAt(int i) {
 		return children.get(i);
 	}
-	
+
 	/**
 	 * Adds child to a list of children of specific node.
-	 * @param node Child to be added. 
+	 * @param node Child to be added.
 	 */
 	public void addChild(TreeNode node) {
 		this.children.add(node);
 	}
 
 	/**
-	 * Method which outputs the textual representation of node's subtree onto stdout.
+	 * Method which outputs the textual representation of node's subtree onto
+	 * stdout.
 	 */
 	public void printSubtree(int indent) {
-		for (int i = 0; i < indent; ++i) System.out.print(" ");
+		for (int i = 0; i < indent; ++i)
+			System.out.print(" ");
 		System.out.println(content);
-		for (TreeNode node : children) node.printSubtree(indent + 1);
+		for (TreeNode node : children)
+			node.printSubtree(indent + 1);
 	}
-	
+
 	/**
 	 * Getter for content field
 	 * @return content
@@ -83,7 +85,7 @@ public class TreeNode {
 	public String getContent() {
 		return this.content;
 	}
-	
+
 	/**
 	 * :)
 	 * @return :)
@@ -91,70 +93,85 @@ public class TreeNode {
 	public boolean isConst() {
 		return isConstant;
 	}
-	
+
 	public boolean isFunction() {
 		return types.size() != 0;
 	}
-	
+
 	public boolean isFunctionDefined() {
-		if (!this.isFunction()) System.err.println("Greska koja se nebi smjela dogodit!");
+		if (!this.isFunction())
+			System.err.println("Greska koja se nebi smjela dogodit!");
 		return this.isDefined;
 	}
-	
+
 	public String getFunctionName() {
-		if (!this.isFunction()) System.err.println("Greska koja se nebi smjela dogodit!");
+		if (!this.isFunction())
+			System.err.println("Greska koja se nebi smjela dogodit!");
 		return this.getChildAt(2).getContent();
- 	}
-	
+	}
+
 	public String getType() {
 		return type;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
-	
+
 	public void setDefined(boolean x) {
 		isDefined = x;
 	}
-	
+
 	public ArrayList<String> getTypes() {
 		return this.types;
 	}
-	
+
 	public ArrayList<String> getNames() {
 		return this.names;
 	}
-	
+
 	public void setTypes(ArrayList<String> types) {
 		this.types = types;
 	}
-	
+
 	public void setNames(ArrayList<String> names) {
 		this.names = names;
 	}
-	
+
 	public void addType(String type) {
 		this.types.add(type);
-	} 
+	}
 
 	public void addName(String name) {
 		this.names.add(name);
-	} 
-	
-	public void setConst() {
-		this.isConst = true;
 	}
-	
+
+	public void setConst() {
+		this.isConstant = true;
+	}
+
+	public boolean getlValue() {
+		return lValue;
+	}
+
+	public void setlValue(boolean lValue) {
+		this.lValue = lValue;
+	}
+
 	@Override
 	public String toString() {
 		String ret = "";
-		for (TreeNode child : children) ret += child.getContent() + " ";
+		for (TreeNode child : children)
+			ret += child.getContent() + " ";
 		return ret;
 	}
-	
+
 }
