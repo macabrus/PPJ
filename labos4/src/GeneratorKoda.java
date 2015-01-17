dbdf1a3f25365dcd8ead164c40506f23e18031bd
Paula <gombarica@gmail.com>
2015-01-17 21:39:45 +0100

    poceci

--- labos4/src/GeneratorKoda.java
+++ labos4/src/GeneratorKoda.java
@@ -1,8 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class GeneratorKoda {
 
-	public static void main(String[] args) {
-		System.out.println("bok");
+	public static TreeNode rootOfGenerativeTree;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<String> inputFileContents = new ArrayList<String>();
+
+		String currLine;
+		while ((currLine = br.readLine()) != null)
+			inputFileContents.add(currLine);
+
+		TreeParser parser = new TreeParser(inputFileContents);
+		rootOfGenerativeTree = parser.getRoot();
+
+		// Provjereno da radi na svim primjerima
+		rootOfGenerativeTree.printSubtree(0);
+
+		System.out.print("\n\tMOVE 40000, R7\n\tJP CMAIN\n");
+//		"\tCALL MAIN\n\tHALT\n\nMAIN\n");
+		
+		// izraz pridruzivanja:
+		System.out.print("\tLOAD R0, (BLA)\n\tPUSH R0\n");
+		
+		// return:
+		System.out.print("\tPOP R6\n\tRET\n");
+		
+		// call main medjulabela
+		
+		
+		// definiraj labele
+		System.out.print("BLA\tDW %D 12\n");
 	}
-	
 }
---
+++ labos4/src/TableNode.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+
+/**
+ * Class which models a table node...
+ * 
+ * @author Ivan Paljak
+ */
+public class TableNode {
+
+	private boolean inLoop = false;
+	
+	private TableNode parent;
+	private ArrayList<TreeNode> declaredStuff; // jos nisam zicer dal je fakat TreeNode
+	
+	/**
+	 * Empty constructor
+	 */
+	public TableNode() {
+		parent = null;
+		declaredStuff = new ArrayList<TreeNode>();
+	}
+	
+	/**
+	 * Constructor from parent
+	 * @param parent
+	 */
+	public TableNode(TableNode parent){
+		this.parent = parent;
+		//if (parent != null) this.inLoop = parent.inLoop;
+		this.declaredStuff = new ArrayList<TreeNode>();
+	}
+
+	public TableNode getParent() {
+		return parent;
+	}
+
+	public ArrayList<TreeNode> getDeclaredStuff() {
+		return declaredStuff;
+	}
+	
+	public void setDeclaredStuff(ArrayList<TreeNode> declaredStuff) {
+		this.declaredStuff = declaredStuff;
+	}
+	
+	public void addChild(TreeNode node) {
+		declaredStuff.add(node);
+	}
+	
+	public void setLoop() {
+		inLoop = true;
+	}
+	
+	public boolean isInLoop() {
+		return inLoop;
+	}
+	
+	
+}
